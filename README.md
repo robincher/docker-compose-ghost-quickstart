b4d3148017b435e530489a4fab380827ea84c0f4
Robin Cher <robin_cher@ida-gds.com>
2018-01-18 15:28:57 +0800

    Resolve upstream conflicts

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # docker-compose-ghost-quickstart
+<<<<<<< HEAD
 Ghost blogging platform quick start with docker compose
+=======
+Quick start docker composition of Ghost blog with Node, NGINX proxy with SSL and mysql database.
+
+## Operating ENV
+- **Operating System** RHEL 5.7 
+
+- **docker** version 1.12.2
+
+- **docker-compose** version 1.9.0
+
+## The Stack
+- Node.js
+    - Ghost blog software
+- NGINX
+    - proxying port 80 calls to the Node web server on port 2368
+- MySQL database
+    - using UTFMB4 encoding (MySQL's UTF8 implementation was limited. UTFMB4 includes Emoji)
+
+## Preparation
+- A server somewhere, like Amazon EC2 , Google Compute Cloud or a virutal machine connected to your own network
+- Register a domain name (either public or within your own network).
+- A pair of SSL certificate and key for your domain.
+- Access to your domain's DNS.
+
+## How to Use It
+0. Clone this project to the server's filesystem.
+0. Edit environment settings in docker-compose.yml
+    - WEB-DOMAIN in ghost service (Your registered domain)
+    - Database settings - in 2 places - under ghost, and under mysql.
+0. Add your SSL certificate and key to ./nginx/ssl 
+0. Set the domain (server_name) in ./nginx/conf/ghost.conf (must match the common name in your SSL certificate)
+0. Run from within your Linux environment or Docker Toolbox environment: 
+    0. docker-compose build
+    0. docker-compose up -d  (you can remove the -d if you want to see logs, then Ctrl+C to stop all containers)
+0. Switch your domain's DNS to point to the address. Go to the domain in our browser and you'll see your new blog.
+0. Go to https://YOUR_DOMAIN/admin to set up your blog.
+0. Once you've made your admin account using that wizard, go back to: https://YOUR_DOMAIN/admin
+0. Log in and enjoy.
+
+## How to back up your database
+0. Run "docker-compose ps" to get a list of running containers.
+0. Locate the name of the mysql container.
+0. Run this command to get the container's internal IP: docker inspect --format='{{.NetworkSettings.IPAddress}}' THAT_CONTAINER-NAME
+0. In your favorite database GUI tool (like Navicat or DataGrip), create a new connection via SSH tunnel to the host machine
+0. Use the internal IP address and database user and password to connect to database once SSH tunnel is established to host.
+0. You'll have access to the data so you can view data and run backups.
+
+
+### To install another theme
+
+Stop docker-compose with:
+```docker-compose stop```
+
+Copy the new theme to ./ghost/content/theme
+
+so that your theme folder sits next to the casper folder in the themes directory
+
+Now run:
+```docker-compose up -d```
+
+Log in to the Ghost admin, go to Settings > General, and at the bottom is the Theme dropdown. Select your theme and click Save.
+
+### How does that work?
+
+The ./ghost/content directory (on docker host machine) gets mounted inside your ghost container at /var/lib/ghost when the container runs. See the docker-compose.yml to see where this mount is happening.
+
+## Security
+
+- Only NGINX's ports (443) are exposed at host level.
+
+## License 
+[MIT LICENSE ](https://github.com/robincher/docker-compose-ghost-quickstart/blob/master/LICENSE)
+
+## References:
++ [Docker Compose for Ghost by John Washam](https://github.com/jwasham/docker-ghost-template)
++ [Ghost @ Dockerhub](https://hub.docker.com/_/ghost/)
+>>>>>>> Initial commits
---
+++ ghost/config.js
@@ -0,0 +1,157 @@
+// # Ghost Configuration
+// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    config;
+
+config = {
+    // ### Production
+    // When running Ghost in the wild, use the production environment.
+    // Configure your URL and mail settings here
+    production: {
+        url: 'http://' + process.env.WEB_DOMAIN,  // do not set to https - your proxy will terminate SSL
+        database: {
+            client: 'mysql',
+            connection: {
+                host:     'mysql',
+                user:     process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_DATABASE,
+                charset:  'utf8mb4'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2368'
+        },
+        // this line was added because using NODE_ENV=production caused permission errors
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        }
+    },
+
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        // Change this to your Ghost blog's published URL.
+        url: 'http://localhost:2368',
+
+        // #### Database
+        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
+
+        //database: {
+        //    client: 'mysql',
+        //    connection: {
+        //        host     : 'mysql',
+        //        user     : process.env.DB_USER,
+        //        password : process.env.DB_PASSWORD,
+        //        database : 'ghost_dev',
+        //        charset  : 'utf8mb4'
+        //    }
+        //},
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-dev.db')
+            },
+            debug: false
+        },
+
+        // Example mail config
+        // Visit http://support.ghost.org/mail for instructions
+        // ```
+        //  mail: {
+        //      transport: 'SMTP',
+        //      options: {
+        //          service: 'Mailgun',
+        //          auth: {
+        //              user: '', // mailgun username
+        //              pass: ''  // mailgun password
+        //          }
+        //      }
+        //  },
+        // ```
+
+        // #### Server
+        // Can be host & port (default), or socket
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: '2368'
+        },
+
+        // #### Paths
+        // Specify where your content directory lives
+        paths: {
+            contentPath: path.join(process.env.GHOST_CONTENT, '/')
+        }
+    },
+
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(process.env.GHOST_CONTENT, '/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing MySQL
+    // Used by Travis - Automated testing run through GitHub
+    'testing-mysql': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '0.0.0.0',
+                user     : 'root',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing pg
+    // Used by Travis - Automated testing run through GitHub
+    'testing-pg': {
+        url: 'http://0.0.0.0:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '0.0.0.0',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2369'
+        },
+        logging: false
+    }
+};
+
+module.exports = config;
