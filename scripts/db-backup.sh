57b1b3ac2181c22da7dec65826253186ca148300
Robin Cher <robin_cher@ida-gds.com>
2018-02-13 10:15:27 +0800

    Ghost Data backup shell script

---
+++ scripts/db-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "#########################################################################"
+echo "## BACKUP GHOST BLOG - `date +%A` - `date +%Y-%m-%d_%Hh%Mm%Ss` ##########"
+echo "#########################################################################"
+
+DAY=$(date +%A)
+FULLDATE=$(date -I)
+
+echo "Creating Backup Folder..."
+mkdir -p /root/backup/ghost
+
+echo "Stopping Ghost Service..."
+docker stop ghost
+
+echo "Backing up Ghost Data Folder..."
+tar -zcvf /root/backup/ghost/ghost-$FULLDATE.tar.gz -C /root/docker-compose-ghost-quickstart/ghost/dbdata
+
+echo "Starting Ghsot Service..."
+docker start blog_ghost_1
+
+echo "## END BACKUP GHOST BLOG - `date +%A` - `date +%Y-%m-%d_%Hh%Mm%Ss` ##########"
